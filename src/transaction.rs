438cc989a7710e8b53f7dacddc36c26f9f1d7b6e
kanet1105 <zombiedelah@gmail.com>
2024-12-24 14:46:02 +0900

    'TransactionType' -> 'Transaction'

--- src/transaction.rs
+++ src/transaction.rs
@@ -3,39 +3,20 @@ use serde::Serialize;
 use serde_json::value::RawValue;
 
 #[derive(Clone, Debug, Serialize)]
-pub enum TransactionType {
+#[serde(untagged)]
+pub enum Transaction {
+    EthRaw(Vec<String>),
     Raw(String),
     Encrypted(String),
 }
 
-#[derive(Clone, Debug, Serialize)]
-pub struct SendEncryptedTransaction {
-    pub rollup_id: String,
-    pub raw_transaction: String,
-}
-
-impl SendEncryptedTransaction {
-    pub const METHOD_NAME: &str = "send_encrypted_transaction";
-}
-
-impl ToRpcParams for SendEncryptedTransaction {
-    fn to_rpc_params(self) -> Result<Option<Box<RawValue>>, serde_json::Error> {
-        let json = serde_json::to_string(&self)?;
-        RawValue::from_string(json).map(Some)
+impl Transaction {
+    pub fn eth_raw_transaction(encoded_transaction: String) -> Self {
+        Self::EthRaw(vec![encoded_transaction])
     }
 }
 
-#[derive(Clone, Debug, Serialize)]
-pub struct SendRawTransaction {
-    pub rollup_id: String,
-    pub raw_transaction: String,
-}
-
-impl SendRawTransaction {
-    pub const METHOD_NAME: &str = "send_raw_transaction";
-}
-
-impl ToRpcParams for SendRawTransaction {
+impl ToRpcParams for Transaction {
     fn to_rpc_params(self) -> Result<Option<Box<RawValue>>, serde_json::Error> {
         let json = serde_json::to_string(&self)?;
         RawValue::from_string(json).map(Some)
