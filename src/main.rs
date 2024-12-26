7ef4506bd8a519d9a12a7f596eeb9ea3c27bc35e
kanet <zombiedelah@gmail.com>
2024-12-27 02:27:01 +0900

    Initializations

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-use std::env;
+use std::{env, time::Duration};
 
 use test_client_rs::{
     self,
-    account::Account,
+    account::{Account, Accounts},
     config::Config,
     connection::{connection_channel, Connection, ConnectionError},
+    generator::{Flag, Generator},
     transaction::Transaction,
 };
-use tokio::task::JoinHandle;
+use tokio::{task::JoinHandle, time::interval};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::fmt().init();
@@ -26,11 +27,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .max_blocking_threads(1)
         .build()?;
 
-    // Initialize accounts from signing keys and set their nonces.
-    let accounts = runtime.block_on(Account::from_config(&config))?;
-
     // Initialize connections
-    let (sender, receiver) = connection_channel(accounts.len());
+    let (sender, receiver) = connection_channel(config.connections());
     let connections = (0..config.connections())
         .map(|_| Connection::new(config.rpc_url(), config.request_timeout(), receiver.clone()))
         .collect::<Result<Vec<Connection>, ConnectionError>>()?;
@@ -38,8 +36,41 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .into_iter()
         .map(|connection| runtime.spawn(connection.init()))
         .collect();
+    tracing::info!("Initialized {} connections.", config.connections());
 
-    // TODO: Create a generator.
+    // Initialize accounts from signing keys and set their nonces.
+    let accounts = runtime.block_on(Account::from_config(&config))?;
+    tracing::info!("Initialized {} accounts.", config.signing_keys().len());
+    accounts.iter().for_each(|account| {
+        tracing::info!(
+            "Address: {:?}\t Nonce: {}",
+            account.address(),
+            account.nonce()
+        )
+    });
+
+    // Initialize the generator.
+    let flag = Flag::default();
+    runtime.spawn(Generator::init(flag.clone(), transaction, accounts, sender));
+    tracing::info!("Initialized the generator.");
+
+    // Initialize the ticker.
+    runtime.spawn({
+        let duration = config.duration();
+        let flag = flag.clone();
+
+        async move {
+            let mut interval = interval(Duration::from_secs(1));
+            interval.tick().await;
+
+            for second in 0..duration {
+                interval.tick().await;
+                tracing::info!("{} seconds passed..", second + 1);
+            }
+
+            flag.stop();
+        }
+    });
 
     runtime.block_on(async move {
         for connection in connection_handles.into_iter() {
@@ -50,6 +81,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-async fn transaction(account: Account) -> Transaction {
+async fn transaction(accounts: Accounts) -> Transaction {
     Transaction::EthRaw(vec!["0x".to_owned()])
 }
